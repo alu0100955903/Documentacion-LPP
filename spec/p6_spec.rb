188c0128e148626e01f7c1a5336eb8b7d1bdc143
Liam Okelly <alu0100955903@ull.edu.es>
2019-11-11 13:29:04 +0000

    Ingesta de referencia acierta

--- spec/p6_spec.rb
+++ spec/p6_spec.rb
@@ -2,18 +2,18 @@ require 'p6/alimento.rb'
 
 RSpec.describe Alimento do
 	before :each do
-		@alimento  = Alimento.new("carne",10,2,6,7,8,"hombre")
-		@alimento2 = Alimento.new("carne",10,2,6,7,8,"mujer")
+		@alimento  = Alimento.new("carne",73,2,300,7,8,"hombre")
+		@alimento2 = Alimento.new("carne",33,2,240,7,8,"mujer")
 	end
 
 	describe "Pruebas alimento" do
 
 		it "Alimento tiene nombre" do
-			expect(@alimento.nombre) == "carne"
+			expect(@alimento.nombre).to eq("carne")
 		end
 
 		it "Alimento tiene proteina" do
-			expect(@alimento.proteinas).to eq(10)
+			expect(@alimento.proteinas).to eq(73)
 		end
 
 		it "Alimento tiene carbohidratos" do
@@ -21,7 +21,7 @@ RSpec.describe Alimento do
 		end
 
 		it "Alimento tiene lipidos" do
-			expect(@alimento.lipidos).to eq(6)
+			expect(@alimento.lipidos).to eq(300)
 		end
 
 		it "Alimento tiene GEI" do
@@ -29,11 +29,11 @@ RSpec.describe Alimento do
 		end
 
 		it "Alimento tiene terreno" do
-			expect(@alimento.terreno) == 8
+			expect(@alimento.terreno)==8
 		end
 
 		it "Genero definido correctamente" do
-			expect(@alimento.genero) == "hombre"
+			expect(@alimento.genero).to eq("hombre")
 		end
 
 		it "Ingesta de energia hombre" do
