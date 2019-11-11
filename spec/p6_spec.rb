41c181523fc4fa5f4bf67ee9d87bf68a1323606f
Liam Okelly <alu0100955903@ull.edu.es>
2019-11-11 11:52:21 +0000

    Falla al preguntar por el ficheor que contiene la clase alimento

--- spec/p6_spec.rb
+++ spec/p6_spec.rb
@@ -1,9 +1,1 @@
-RSpec.describe P6 do
-  it "has a version number" do
-    expect(P6::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
+require 'alimento.rb'
