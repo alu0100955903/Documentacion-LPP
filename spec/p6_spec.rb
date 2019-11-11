12393ef84b79df8bc2866330a394fddeaf810a13
Liam Okelly <alu0100955903@ull.edu.es>
2019-11-11 12:26:38 +0000

    Creamos prueba de nombre y falla

--- spec/p6_spec.rb
+++ spec/p6_spec.rb
@@ -1,1 +1,13 @@
-require 'alimento.rb'
+require 'p6/alimento.rb'
+
+RSpec.describe Alimento do
+	before :each do
+		@alimento = Alimento.new("carne",10,2,6)
+	end
+
+	describe "Pruebas alimento" do
+		it "Alimento tiene nombre" do
+			expect(@alimento.nombre) == "carne"
+		end
+	end
+end
