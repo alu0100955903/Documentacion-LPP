a45fb46622f5904663397b28ad1d4b011464209f
Liam Okelly <alu0100955903@ull.edu.es>
2019-11-18 14:01:14 +0000

    Hacemos el test para insertar cabeza [ERROR]

--- spec/lista_spec.rb
+++ spec/lista_spec.rb
@@ -10,15 +10,20 @@ RSpec.describe Alimento do
 
 	describe "Pruebas de lista" do
 		it "Inserta nodo por cola bien" do
-			@lista.inserta_por_cola(@nodo1)
+			@lista.inserta_por_cola(1)
 			expect(@lista.tail.value).to eq(1)
 		end
 
 		it "Extrae nodo por cola bien" do
-			@lista.inserta_por_cola(@nodo1)
-			@lista.inserta_por_ccola(@nodo2)
+			@lista.inserta_por_cola(1)
+			@lista.inserta_por_ccola(2)
 			@lista.extraer_por_cola
 			expect(@lista.tail.value).to eq(2)
+		end
+
+		it "Inserta nodo por cabeza bien" do
+			@lista.inserta_por_cabeza(1)
+			expect(@lista.head.value).to eq(1)
 		end
 	end
 end
