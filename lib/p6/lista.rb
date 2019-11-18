077258d88f7b5ff45e8fb6f7ea6bc323b9d34d07
Liam Okelly <alu0100955903@ull.edu.es>
2019-11-18 13:47:10 +0000

    Hacemos la funcion insertar [ACIERTO]

--- lib/p6/lista.rb
+++ lib/p6/lista.rb
@@ -7,4 +7,17 @@ class List
 		@head = nil
 		@tail = nil
 	end
+
+	def insertar_por_cola(value)
+		nodo=Node.new(value,nil,nil)
+		if(@tail==nil)
+			@tail=nodo
+			@head=nodo
+		else
+			nodo.prev=@tail
+			@tail.nest=nodo
+			@tail=nodo
+			nodo.nest=nil
+		end
+	end
 end
