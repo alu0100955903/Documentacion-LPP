4dd546d31b4d06e1e9d7a97b31cb8d223b262ba3
Liam Okelly <alu0100955903@ull.edu.es>
2019-11-18 14:14:54 +0000

    Hacemos el test para lista vacia [ERROR]

--- spec/lista_spec.rb
+++ spec/lista_spec.rb
@@ -36,5 +36,11 @@ RSpec.describe Alimento do
 			@lista.extraer_por_cabeza
 			expect(@lista.head.value).to eq(2)
 		end
+
+		it "Correcto: comprueba si está vacia" do
+			@lista.inserta_por_cola(2)
+			@lista.extraer_por_cola
+			expect(@lista.vacio).to be(true)
+		end
 	end
 end
