26fd2b6b62b9c57114dc7e014862c54b4be8c131
Liam Okelly <alu0100955903@ull.edu.es>
2019-11-18 13:51:15 +0000

    Hacemos el test para extraer [ERROR]

--- spec/lista_spec.rb
+++ spec/lista_spec.rb
@@ -9,9 +9,16 @@ RSpec.describe Alimento do
 	end
 
 	describe "Pruebas de lista" do
-		it "Inserta nodo bien" do
+		it "Inserta nodo por cola bien" do
 			@lista.inserta_por_cola(@nodo1)
 			expect(@lista.tail.value).to eq(1)
+		end
+
+		it "Extrae nodo por cola bien" do
+			@lista.inserta_por_cola(@nodo1)
+			@lista.inserta_por_ccola(@nodo2)
+			@lista.extraer_por_cola
+			expect(@lista.tail.value).to eq(2)
 		end
 	end
 end
