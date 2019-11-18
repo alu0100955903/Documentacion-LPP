8d797de0ab48fee1bb1e6c78041c74cb77d15a03
Liam Okelly <alu0100955903@ull.edu.es>
2019-11-18 13:42:55 +0000

    Hacemos el test para insertar [ERROR]

--- spec/lista_spec.rb
+++ spec/lista_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe Alimento do
 	before :each do
 		@nodo1 = Node.new(1,NIL,NIL)
 		@nodo2 = Node.new(2,NIL,NIL)
+		@lista = List.new(NIL,NIL)
+	end
+
+	describe "Pruebas de lista" do
+		it "Inserta nodo bien" do
+			@lista.inserta_por_cola(@nodo1)
+			expect(@lista.tail.value).to eq(1)
+		end
 	end
 end
