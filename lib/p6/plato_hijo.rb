20349f30b9b2b3290289d4d46ea475f8543eb1ff
Liam Okelly <alu0100955903@ull.edu.es>
2019-11-26 19:48:59 +0000

    Arreglando plato hijo

--- lib/p6/plato_hijo.rb
+++ lib/p6/plato_hijo.rb
@@ -1,16 +1,8 @@
 class Plato_hijo < Plato
 
-	include Comparable, Enumerable
+	include Comparable
 
 	def <=> (other)
 		por_proteinas <=> other.por_proteinas
 	end
-
-	def each(&block)
-		puntero=@alimento.head
-		while(puntero!=nil) do
-			yield puntero.value
-			puntero=puntero.nest
-		end
-	end	
 end
